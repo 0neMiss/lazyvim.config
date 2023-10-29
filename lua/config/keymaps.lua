5c62d6a989e9c722b53969f08b94ad4d6521d838
Shehane <jordan.shehane@charter.com>
2023-10-29 14:01:25 -0600

    making keymaps work in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
 vim.keymap.set(
   "n",
   "<leader>sx",
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "Resume" }
 )
+-- Normal Mode
 vim.keymap.set("n", "L", "$")
 vim.keymap.set("n", "H", "^")
+
+-- Visual Mode
+vim.keymap.set("v", "L", "$")
+vim.keymap.set("v", "H", "^")
