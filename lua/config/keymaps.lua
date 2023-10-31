ca920aa54b0d26bd1e7397db6a6faffb8580061a
Shehane <jordan.shehane@charter.com>
2023-10-31 16:35:12 -0600

    updating keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,11 +8,22 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Resume" }
 )
 
-vim.keymap.set({ "v", "n" }, "L", "$")
-vim.keymap.set({ "v", "n" }, "H", "^")
+-- L and H go to end of line
+vim.keymap.set({ "v", "n" }, "L", "^")
+vim.keymap.set({ "v", "n" }, "H", "$")
 
-vim.keymap.set({ "v", "n" }, ";", ":")
+-- so that delete and change work correctly with L and H
 vim.keymap.set({ "v", "n" }, "dL", "d$")
 vim.keymap.set({ "v", "n" }, "dH", "d^")
 vim.keymap.set({ "v", "n" }, "cL", "c$")
 vim.keymap.set({ "v", "n" }, "cH", "c^")
+
+-- center the cursor
+vim.keymap.set({ "v", "n" }, "<", "^zz")
+vim.keymap.set({ "v", "n" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "v", "n" }, "<C-u>", "<C-u>zz")
+vim.keymap.set({ "v", "n" }, "j", "jzz")
+vim.keymap.set({ "v", "n" }, "k", "kzz")
+
+-- faster commands
+vim.keymap.set({ "v", "n" }, ";", ":")
