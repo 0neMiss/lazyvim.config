71c0d3acb95eb97522161a24ed14781d96ac9793
Shehane <jordan.shehane@charter.com>
2023-11-05 07:31:55 -0700

    some cool stuff to search last yank

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -6,6 +6,7 @@ return {
       ":Telescope file_browser path=%:p:h=%:p:h<cr>",
       desc = "Browse Files",
     },
+    { "<leader>s*", ":Telescope live_grep<cr><C-r>*", desc = "Grep for last yank" },
   },
   config = function()
     require("telescope").load_extension("file_browser")
