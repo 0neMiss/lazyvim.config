3da5113637838ef674ea3a54ec7c48736556a5d2
Shehane <jordan.shehane@charter.com>
2023-11-05 22:30:24 -0700

    Case sensitive search wasn't working ;_;

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
-
 vim.keymap.set(
   "n",
   "<leader>sx",
@@ -17,19 +16,21 @@ vim.keymap.set({ "v" }, "$", "y/<C-r>0")
 
 -- mark the ' register for a easy jumpback
 vim.keymap.set({ "v", "n" }, "M", "m'")
---  Case sensitive search and replace the current selections
+
+--  Search and replace the current selections
 --  Visual Mode
-vim.keymap.set({ "v" }, "<C-$>", [[y:%s\C/<C-r>0/]])
+vim.keymap.set({ "v" }, "<C-$>", [[y:%s/<C-r>0/]])
 
 -- Normal Mode (select current word instead)
-vim.keymap.set({ "n" }, "<C-$>", [[yiw:%s\C/<C-r>0/]])
+vim.keymap.set({ "n" }, "<C-$>", [[yiw:%s/<C-r>0/]])
 
--- so that delete and change work correctly with L and H
+-- So that delete and change work correctly with L and H
 vim.keymap.set({ "v", "n" }, "dL", "d$")
 vim.keymap.set({ "v", "n" }, "dH", "d^")
 vim.keymap.set({ "v", "n" }, "cL", "c$")
 vim.keymap.set({ "v", "n" }, "cH", "c^")
--- center the cursor
+
+-- center the cursor (might be causing some weird issues in iterm)
 vim.keymap.set({ "v", "n" }, "<", "^zz")
 vim.keymap.set({ "v", "n" }, "<C-d>", "<C-d>zz")
 vim.keymap.set({ "v", "n" }, "<C-u>", "<C-u>zz")
