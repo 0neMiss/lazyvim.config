698acf84149f7fb8c32ece82810f5f8ef514a06a
Shehane <jordan.shehane@charter.com>
2023-11-23 12:24:24 -0700

    getting rid of error messages on startup from bad imports

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
