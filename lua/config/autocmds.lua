f55f7a6b954a22cedda8d8449654ff2d1ea7c2d1
Shehane <jordan.shehane@charter.com>
2023-11-23 10:16:05 -0700

    auto resize windows when switching between them

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Add any additional autocmds here
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+
+-- Resize a windows when i enter them
+vim.api.nvim_create_autocmd({ "WinEnter" }, {
+  callback = function()
+    vim.cmd([[execute "normal \<C-w>="]])
+  end,
+})
