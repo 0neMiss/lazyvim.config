44875e7204ac679922bc9f3eae5024034246f15d
0neMiss <jordanshehane@gmail.com>
2023-12-10 11:40:09 -0700

    sick of the italics on comments.

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("catppuccin").setup({
+  color_overrides = {
+    transparent_background = true,
+    no_italic = true,
+  },
+})
